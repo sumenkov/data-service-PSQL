e6a7c4eaa0b979721b9c88420aa46f0757b0d3f8
sdv <sumenkov.dv@yandex.ru>
2022-11-10 20:50:23 +0700

    Получил из БД статистику за период

--- oldFiles/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.sumenkov.dspsql.repository;
-
-import ru.sumenkov.dspsql.model.db.ProductsModel;
-
-import java.util.List;
-
-public interface ProductsRepository {
-    List<ProductsModel> getProducts();
-}
