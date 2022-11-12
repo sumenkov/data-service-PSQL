c975997314ee9042e86581662e4687d7213628c1
sdv <sumenkov.dv@yandex.ru>
2022-11-12 16:45:43 +0700

    модель для ошибок готова, осталось расставить регистрацию ошибок, запись в файл, и выход из программы

--- src/main/java/ru/sumenkov/dspsql/model/ErrorModel.java
+++ src/main/java/ru/sumenkov/dspsql/model/ErrorModel.java
@@ -1,11 +1,15 @@
 package ru.sumenkov.dspsql.model;
 
 public class ErrorModel {
-    private static final String TYPE = "error";
+    private final String TYPE = "error";
     private String message;
 
     public ErrorModel(String message) {
         this.message = message;
+    }
+
+    public String getTYPE() {
+        return TYPE;
     }
 
     public String getMessage() {
